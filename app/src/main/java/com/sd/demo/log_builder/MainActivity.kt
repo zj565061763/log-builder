9e72d58387802fe00cc50a25008b40f8da543d33
zj565061763 <565061763@qq.com>
2023-06-06 18:23:05 +0800

    update

--- app/src/main/java/com/sd/demo/log_builder/MainActivity.kt
+++ app/src/main/java/com/sd/demo/log_builder/MainActivity.kt
@@ -3,45 +3,28 @@ package com.sd.demo.log_builder
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.Window
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.sd.demo.log_builder.ui.theme.AppTheme
+import androidx.appcompat.app.AppCompatActivity
 import com.sd.lib.logbuilder.FLogBuilder
 import java.util.UUID
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btn).setOnClickListener { view ->
+            logMsg {
+                FLogBuilder().add("onClick").nextLine()
+                    .pair("view", view).nextLine()
+                    .pairHash("view hash", view).nextLine()
+                    .pairStr("view string", view).nextLine()
+                    .instance(view).nextLine()
+                    .instanceStr(view).nextLine()
+                    .uuid(UUID.randomUUID().toString())
+            }
+        }
+
         logMsg {
             FLogBuilder().add("onCreate").instance(this@MainActivity)
-        }
-        val view = findViewById<View>(Window.ID_ANDROID_CONTENT)
-        setContent {
-            AppTheme {
-                Content(
-                    onClick = {
-                        logMsg {
-                            FLogBuilder().add("onClick").nextLine()
-                                .pair("view", view).nextLine()
-                                .pairHash("view hash", view).nextLine()
-                                .pairStr("view string", view).nextLine()
-                                .instance(view).nextLine()
-                                .instanceStr(view).nextLine()
-                                .uuid(UUID.randomUUID().toString())
-                        }
-                    },
-                )
-            }
         }
     }
 
@@ -63,23 +46,6 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         logMsg {
             FLogBuilder().add("onDestroy").instance(this@MainActivity)
-        }
-    }
-}
-
-@Composable
-private fun Content(
-    onClick: () -> Unit,
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        Button(
-            onClick = onClick
-        ) {
-            Text(text = "button")
         }
     }
 }
