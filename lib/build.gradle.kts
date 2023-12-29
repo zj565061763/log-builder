568f0cdb1ffb3f8c12fd58afa6a6bbf51edbf00c
zjun <565061763@qq.com>
2023-12-29 22:08:49 +0800

    1.2.7

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "log-builder"
-val libVersionName = "1.2.6"
+val libVersionName = "1.2.7"
 
 android {
     namespace = "com.sd.lib.log.builder"
